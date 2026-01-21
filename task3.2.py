0943fc8f89d2c6b4402657749a14bc76c0ce366a
agrawalaarush09-cmyk <agrawalaarush09@gmail.com>
2026-01-21 22:58:51 +0530

    Add files via upload

---
+++ task3.2.py
@@ -0,0 +1,16 @@
+import math
+
+n=int(input("enter a number:"))
+def square(n):
+    return n*n
+result= square(n)
+print("square of",n,"is",result)
+
+
+
+log= math.log(n)
+sin = math.sin(n) #input in  radians
+
+
+print("logarithm of",n,"is",log)
+print(sin)
